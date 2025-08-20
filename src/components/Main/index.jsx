417ab29cfd05fafb15918fe2b917624fafa9c8b4
NikRedFox <nikolasbs11@gmail.com>
2025-08-20 17:10:14 -0300

    Geometry test

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -15,11 +15,18 @@ export default function Main() {
       <div className={styles.tint}></div>
       <h1 className={styles.title}>CSS Modules</h1>
       <h2 className={styles.subTitle}>O que é? O que faz? Como usa? </h2>
-      <div className={styles.container}>        
-        <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
-        <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
-        <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
+      <div className={styles.container}> 
+        <div className={styles.paragraphContainer}>
+          <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
+        </div>       
+        <div className={styles.paragraphContainer}>
+          <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
+        </div>       
+        <div className={styles.paragraphContainer}>
+          <p className={styles.paragraph}>CSS Modules é um sistema que gera nomes únicos para classes, evitando conflitos de estilos.</p>
+        </div>       
       </div>
+      <div className={styles.hexagono}></div>
       
     </main>
   );
